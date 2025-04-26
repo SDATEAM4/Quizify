77e746f23250a787acdca3b39ec6c53fc4e21707
A-noob-in-Coding <naeemabdulrehman77@gmail.com>
2025-04-26 17:26:58 +0500

    [FRONTEND] [USER] integrated backend with homepage

--- frontend/src/components/courseCard.jsx
+++ frontend/src/components/courseCard.jsx
@@ -1,14 +1,12 @@
 export const CourseCard = ({ 
   title, 
-  description, 
-  imageUrl, 
-  iconClass,
-  onClick 
+  description,
+  imageUrl
 }) => {
   return (
     <div 
       className="bg-white rounded-lg shadow-sm border border-gray-200 overflow-hidden transition-transform duration-300 hover:shadow-md hover:scale-[1.01] cursor-pointer"
-      onClick={onClick}
+      
     >
       {/* Course Image */}
       <div className="h-40 overflow-hidden">
@@ -23,7 +21,6 @@ export const CourseCard = ({
       <div className="p-4">
         {/* Title and Icon */}
         <div className="flex items-center mb-3">
-          <i className={`${iconClass} text-black mr-3 text-xl`}></i>
           <h3 className="font-medium text-gray-800">{title}</h3>
         </div>
         
