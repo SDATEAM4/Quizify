fc82957ee869690f1c4b994fe176a7493abdf398
syed-ali3 <aliahmad4142@gmail.com>
2025-04-28 00:33:49 +0500

    [FRONTEND] minor changes

--- frontend/src/components/SelectionControls.jsx
+++ frontend/src/components/SelectionControls.jsx
@@ -68,23 +68,23 @@ const SelectionControls = ({
             <div className="md:col-span-2 flex space-x-4 mt-2">
                 <button
                     onClick={() => onChartChange('bar')}
-                    className={`px-4 py-2 rounded-md transition-colors duration-200 ${activeChart === 'bar'
+                    className={`hover:cursor-pointer px-4 py-2 rounded-md transition-colors duration-200 ${activeChart === 'bar'
                         ? 'bg-blue-600 text-white'
                         : 'bg-white text-gray-700 border border-gray-300 hover:bg-gray-50'
                         }`}
                     disabled={!selectedQuiz}
                 >
-                    Quiz
+                    Marks
                 </button>
                 <button
                     onClick={() => onChartChange('pie')}
-                    className={`px-4 py-2 rounded-md transition-colors duration-200 ${activeChart === 'pie'
+                    className={`hover:cursor-pointer px-4 py-2 rounded-md transition-colors duration-200 ${activeChart === 'pie'
                         ? 'bg-blue-600 text-white'
                         : 'bg-white text-gray-700 border border-gray-300 hover:bg-gray-50'
                         }`}
                     disabled={!selectedQuiz}
                 >
-                    Charts
+                    Performance
                 </button>
             </div>
         </div>
